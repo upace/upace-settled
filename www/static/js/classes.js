aee7ab5115a553fbc0e30aa4736bf1db2a73d1e8
Andrew Clarke <pendensproditor@gmail.com>
2015-02-01 17:42:37 -0500

    reservedByMe flags now working

--- www/static/js/classes.js
+++ www/static/js/classes.js
@@ -41,7 +41,7 @@
                         classesByDate = a;
                         myReservedClassSlots = [];
                         for (var i = 0; i < b.length; i++) {
-                            myReservedClassSlots.push(b[i].get('slot'));
+                            myReservedClassSlots.push(b[i].get('slotId'));
                         }
                         renderClasses();
                     } else {
