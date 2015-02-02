bebd9e3a8911c599b5b0a7ce09fc7c16cbd09d88
Kevin Foster <gubdynasty@yahoo.com>
2015-02-02 00:16:41 -0500

    Tweaks to start time filter.

--- www/static/js/classes.js
+++ www/static/js/classes.js
@@ -4,6 +4,7 @@
         classesByDate,
         myReservedClassSlots,
         classDetails,
+        startTime,
 
         selectors = {
             'classListings' : '#class-listings',
@@ -13,7 +14,10 @@
             'multiSelectButtons' : '.multi-select-btn',
             'multiCancelButtons' : '.multi-select-cancel-all-btn',
             'multiReserveButtons' : '.multi-select-reserve-all-btn',
-            'reserveTool' : '#collapse-reserve'
+            'reserveTool' : '#collapse-reserve',
+            'startTimeFilterWrap' : '#collapse-add-time',
+            'startTimeFilterForm' : '.listing-enter-time-form',
+            'startTimeFilterInput' : '.form-add-time'
         },
 
         classes = {
@@ -29,8 +33,11 @@
         // cache selectors where we can
         $classListings = $(selectors.classListings),
         $reserveTool = $(selectors.reserveTool),
+        $startTimeFilterWrap = $(selectors.startTimeFilterWrap),
+        $startTimeFilterForm = $(selectors.startTimeFilterForm),
 
         spinner = '<div class="spinner"></div>',
+        startTimeFilterRegex = new RegExp(/^((1[0-2])|^(0?[0-9])):[0-5][0-9](\s?)(pm|am)$/ig),
 
         initializeClasses = function() {
 
@@ -40,11 +47,15 @@
             $(document).on('click', selectors.multiCancelButtons, onMultiCancel);
             $(document).on('click', selectors.multiReserveButtons, onMultiReserve);
             $reserveTool.on('hide.bs.collapse', onCollapseReserveTool);
+            $startTimeFilterWrap.on('show.bs.collapse', onOpenStartTimeFilter);
+            $startTimeFilterWrap.on('hide.bs.collapse', onCloseStartTimeFilter);
+            $startTimeFilterForm.on('submit', onStartTimeFormSubmit);
 
             var date = getUrlParameter('dt');
             if (!date) {
                 date = formatDateForParse(new Date());
             }
+            resetStartTime();
             refreshClasses(date);
         },
 
@@ -58,6 +69,9 @@
                     if(a.length) {
                         classesByDate = a;
 						classesByDate.sort(sortParseResultsByStartTime);
+                        if(startTime) {
+                            classesByDate = filterParseResultsByStartTime(classesByDate, startTime);
+                        }
                         myReservedClassSlots = [];
                         for (var i = 0; i < b.length; i++) {
                             myReservedClassSlots.push(b[i].get('slotId'));
@@ -148,11 +162,47 @@
             if(!start && !end){
                 return '';
             }
-            var s = start.replace(/\s+|PM|AM/g, ''),
+            var s = start.replace(/\s+|pm|am/gi, ''),
                 e = end.replace(/\s+/g, '');
             if (s.charAt(0) === '0') s = s.substr(1);
             if (e.charAt(0) === '0') e = e.substr(1);
             return s + '-' + e;
+        },
+
+        onOpenStartTimeFilter = function() {
+            $('html, body').animate({ scrollTop: 0 }, 200);
+        },
+
+        onCloseStartTimeFilter = function() {
+            $(selectors.startTimeFilterInput).val('');
+            resetStartTime();
+        },
+
+        resetStartTime = function() {
+            var d = new Date();
+            if(isToday(d)) {
+                var h = d.getHours(),
+                    ampm = (h >= 12)? 'PM' : 'AM',
+                    h = ((h + 11) % 12 + 1);
+                startTime = h + ':' + d.getMinutes() + ' ' + ampm;
+            } else {
+                startTime = null;
+            }
+        },
+
+        onStartTimeFormSubmit = function(e) {
+            e.preventDefault();
+            var val = $(e.currentTarget).find(selectors.startTimeFilterInput).val();
+            if(!val) {
+                startTime = null;
+                $(selectors.startTimeFilterWrap).collapse('hide');
+            } else if(val.match(startTimeFilterRegex)) {
+                if(val.indexOf(' ') == -1) {
+                    // add a space before AM/PM since Parse is picky
+                    val = val.slice(0, -2) + ' ' + val.slice(-2);
+                }
+                startTime = val;
+            }
         };
 
         initializeClasses();
